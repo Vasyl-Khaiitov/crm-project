833b05fe13f97104dd77da382d5bea228fa9adda
MazaFaaka <haitovvasyl@gmail.com>
2025-10-01 19:45:09 +0200

    ccompled added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from '@/components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello Vasias this Home Page</h1>
+      <h1 className="text-xl">Home Page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
