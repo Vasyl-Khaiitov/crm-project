9b154b44b4f8978df7c1e72d8ee7991144530f71
MazaFaaka <haitovvasyl@gmail.com>
2025-10-16 16:37:55 +0200

    added: added useEffect or page single

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,11 +1,22 @@
+'use client';
+
 import Header from '@/app/components/header';
-import React from 'react';
+import { notFound } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
+  useEffect(() => {
+    const id = Number.parseInt(params.id);
+
+    if (Number.isNaN(id)) {
+      notFound();
+    }
+  }, [params.id]);
+
   return (
     <>
       <Header>Company ({params.id})</Header>
